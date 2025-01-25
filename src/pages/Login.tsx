69972473bf1f4652e13fb87487a4bf59795c1912
JPChoyon <kotjpckhan@gmail.com>
2025-01-25 20:35:12 +0600

    user added successfully and access token decoded also added

---
+++ src/pages/Login.tsx
@@ -0,0 +1,55 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/feature/auth/AuthApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/feature/auth/AuthSlice";
+import { verifyToken } from "../utils/verifyToken";
+
+const Login = () => {
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "admin123",
+    },
+  });
+  const [login, { error }] = useLoginMutation();
+  // console.log("data =>", data);
+  // console.log("error =>", error);
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className=" relative ">
+        <input
+          type="text"
+          id="id"
+          className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+          placeholder="Your Id"
+          {...register("id")}
+        />
+      </div>
+      <div className=" relative ">
+        <input
+          type="text"
+          id="password"
+          className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+          placeholder="Password"
+          {...register("password")}
+        />
+      </div>
+      <Button htmlType="submit">Log In</Button>
+    </form>
+  );
+};
+
+export default Login;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { routerGenerator } from "../utils/routesGenerator";
 import { adminPaths } from "./Admin.route";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
     path: "/student",
     element: <App />,
     children: routerGenerator(adminPaths),
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
